a8641844edd5d58a879397c1807c244203123d0a
schmidt <schmidt.ajax@gmail.com>
2019-12-15 16:42:13 +0700

    testEquality пока завершается неудачей

--- src/DollarTest.java
+++ src/DollarTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class DollarTest {
 	@Test
@@ -10,5 +11,10 @@ public class DollarTest {
     	assertEquals(10, product.amount);
     	product = five.times(3);
     	assertEquals(15, product.amount);
+	}
+	
+	@Test
+	public void testEquality() {
+		assertTrue((new Dollar(5)).equals(new Dollar(5)));
 	}
 }
