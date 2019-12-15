66816ce1874b145440c56b8649e262f38adc2596
schmidt <schmidt.ajax@gmail.com>
2019-12-15 22:08:49 +0700

    Избавились от пустых классов-наследников
    Избавились от лишних тестов

--- src/Money.java
+++ src/Money.java
@@ -19,12 +19,12 @@ class Money {
 				currency().equals(money.currency());
 	}
 	
-	static Dollar dollar(int amount) {
-		return new Dollar(amount, "USD");
+	static Money dollar(int amount) {
+		return new Money(amount, "USD");
 	}
 	
-	static Franc franc(int amount) {
-		return new Franc(amount, "CHF");
+	static Money franc(int amount) {
+		return new Money(amount, "CHF");
 	}
 	
 	public String toString() {
--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -4,14 +4,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
-public class DollarTest {
+public class MoneyTest {
 	@Test
 	public void testMultiplication() {
     	Money five = Money.dollar(5);
     	assertEquals(Money.dollar(10), five.times(2));
     	assertEquals(Money.dollar(15), five.times(3));
-		assertTrue(Money.franc(5).equals(Money.franc(5)));
-		assertFalse(Money.franc(5).equals(Money.franc(6)));
 		assertFalse((Money.dollar(5)).equals(Money.franc(5)));
 	}
 	
@@ -27,8 +25,4 @@ public class DollarTest {
 		assertEquals("CHF", Money.franc(1).currency());
 	}
 	
-	@Test
-	public void testDifferentClassEquality() {
-		assertTrue(new Money(10, "USD").equals(new Dollar(10, "USD")));
-	}
 }
