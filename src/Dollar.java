1fe60387fe1f9510ae195630602d9df777ac3145
schmidt <schmidt.ajax@gmail.com>
2019-12-15 21:03:05 +0700

    Добавили переменную Money.currency и инициализируем ее в конструкторах-наследниках

--- src/Dollar.java
+++ src/Dollar.java
@@ -2,15 +2,11 @@
 public class Dollar extends Money {
 	Dollar(int amount) {
 		this.amount = amount;
+		this.currency = "USD";
 	}
 	
 	@Override
 	Money times(int multiplier) {
 		return new Dollar(amount * multiplier);
-	}
-
-	@Override
-	String currency() {
-		return "USD";
 	}
 }
--- src/Franc.java
+++ src/Franc.java
@@ -2,15 +2,11 @@
 public class Franc extends Money {
 	Franc(int amount) {
 		this.amount = amount;
+		this.currency = "CHF"; 
 	}
 	
 	@Override
 	Money times(int multiplier) {
 		return new Franc(amount * multiplier);
-	}
-
-	@Override
-	String currency() {
-		return "CHF";
 	}
 }
