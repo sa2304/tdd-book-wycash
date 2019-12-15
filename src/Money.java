44e924b8bf5788da41ce22a014da33bc8e57e4c4
schmidt <schmidt.ajax@gmail.com>
2019-12-16 00:03:23 +0700

    Добавили тест testPlusReturnsSum()

--- src/Money.java
+++ src/Money.java
@@ -20,7 +20,7 @@ class Money implements Expression {
 	}
 	
 	Expression plus(Money addend) {
-		return new Money(amount + addend.amount, currency);
+		return new Sum(this, addend);
 	}
 	
 	static Money dollar(int amount) {
---
+++ src/Sum.java
@@ -0,0 +1,9 @@
+
+public class Sum implements Expression {
+	Sum(Money augend, Money addend) {
+		
+	}
+	
+	Money augend;
+	Money addend;
+}
