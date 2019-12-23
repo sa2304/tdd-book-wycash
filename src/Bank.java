fa650295974f0bdf647dcc3099340a654050597d
schmidt <schmidt.ajax@gmail.com>
2019-12-24 00:53:55 +0700

    А что, если аргументом Bank.reduce() будет Money?
    reduce() не может приводит выражения к выбранной валюье, если не знает
    обменного курса. Возможно в будущем в него будем передавать Bank.

--- src/Bank.java
+++ src/Bank.java
@@ -1,6 +1,10 @@
 
 public class Bank {
 	Money reduce(Expression source, String to) {
+		if (source instanceof Money) {
+			return (Money) source;
+		}
+		
 		Sum sum = (Sum) source;
 		return sum.reduce(to);
 	}
